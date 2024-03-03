e1cefbb6375f8e4aa38a17e07bab921785984a2f
MickaelMoon <mickael.saintmartin@outlook.fr>
2024-03-03 17:45:22 +0100

    Added CardsList path route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Quizz from "./pages/Quizz/Quizz";
+import CardsList from "./pages/CardsList/CardsList";
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/quizz" element={<Quizz/>}/>
+          <Route path="/cards" element={<CardsList/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </Router>
