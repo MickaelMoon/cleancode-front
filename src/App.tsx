e0b0d49eff7303e8ba7b1c78fc99103cc021a23b
MickaelMoon <mickael.saintmartin@outlook.fr>
2024-03-03 17:31:38 +0100

    Created NotFound page Changed some css lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
 
 
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </Router>
 
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import './NotFound.css';
+
+function NotFound() {
+
+    return (
+        <div className="page-not-found">
+            <p>Page not found.</p>
+        </div>
+    );
+
+}
+
+export default NotFound;
